d4c3b114f60ccea5e1f98c3c827e921c68a59bac
seoil0404 <seoil6795@gmail.com>
2026-01-12 15:02:16 +0900

    Add PlayerStatusView

---
+++ Assets/_Project/Runtime/UI/Widgets/PlayerStatusView/Scripts/PlayerStatusView.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatusView : MonoBehaviour
+{
+    public static PlayerStatusView Instance {  get; private set; }
+
+    [SerializeField] private Image healthView;
+    [SerializeField] private Image manaView;
+    [SerializeField] private float smoothSpeed = 5f;
+
+    private float healthFillAmount = 1;
+    private float manaFillAmount = 1;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetHealthView(float amount)
+    {
+        healthFillAmount = amount;
+    }
+
+    public void SetManaView(float amount)
+    {
+        manaFillAmount = amount;
+    }
+
+    private void Update()
+    {
+        healthView.fillAmount = Mathf.Lerp(healthView.fillAmount, healthFillAmount, Time.deltaTime * smoothSpeed);
+        manaView.fillAmount = Mathf.Lerp(manaView.fillAmount, manaFillAmount, Time.deltaTime * smoothSpeed);
+    }
+}
