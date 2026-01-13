e33d6ef9637460b491b698a9665d3d8e173c0254
esky1004 <esky1004@gmail.com>
2026-01-13 22:08:15 +0900

    Enemy Default

---
+++ Assets/_Project/Runtime/Features/Enemy/Scripts/EnemyData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public enum EnemyGrade
+{
+    A, B, C, D, F
+}
+
+
+[CreateAssetMenu(fileName = "EnemyData_", menuName = "Game/Enemy Data")]
+public class EnemyDataSO : ScriptableObject
+{
+    public EnemyGrade grade = EnemyGrade.F;
+
+    public int maxHP = 10;
+    public int damage = 1;
+
+    public float moveSpeed = 2f;
+    public float moveDistance = 3f;
+    public float restTime = 1f;
+
+    public float fixedZ = 0f;
+}
+
+
