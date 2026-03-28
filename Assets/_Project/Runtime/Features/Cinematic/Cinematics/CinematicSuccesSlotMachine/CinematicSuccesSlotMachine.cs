240f26e924dbbbbfb3577eff7a604ab8ca0a5053
seoil0404 <seoil6795@gmail.com>
2026-03-28 12:54:16 +0900

    Update SlotMachine

--- Assets/_Project/Runtime/Features/Cinematic/Cinematics/CinematicSuccesSlotMachine/CinematicSuccesSlotMachine.cs
+++ Assets/_Project/Runtime/Features/Cinematic/Cinematics/CinematicSuccesSlotMachine/CinematicSuccesSlotMachine.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class CinematicSuccesSlotMachine : Cinematic
 {
     [SerializeField] private List<DialogData> dialogDatas;
+    [SerializeField] private VisualEffect visualEffectPrefab;
+
+    private VisualEffect visualEffect = null;
 
     public override void Play()
     {
@@ -13,6 +18,18 @@ public class CinematicSuccesSlotMachine : Cinematic
         var cinematicDialog = CinematicManager.Show<CinematicDialog>();
         cinematicDialog.BindDialog(dialogDatas[randomIndex]);
         cinematicDialog.OnFinished += Finish;
+
+        visualEffect = Instantiate(visualEffectPrefab);
+        visualEffect.Play();
+
+        StartCoroutine(StopVisualEffect());
+    }
+
+    private IEnumerator StopVisualEffect()
+    {
+        yield return new WaitForSeconds(1f);
+
+        visualEffect.Stop();
     }
 
     private void Finish(Cinematic obj)
