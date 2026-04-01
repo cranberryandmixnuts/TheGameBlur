822a5de9dd5a2621b25402ee03db6dd2b29de844
jaehoo0305 <grooochyjeff1234@gmail.com>
2026-04-01 23:08:10 +0900

    Enemy Fixed

--- Assets/_Project/Runtime/Features/Enemy/EnemySpawner/EnemySpawner.cs
+++ Assets/_Project/Runtime/Features/Enemy/EnemySpawner/EnemySpawner.cs
@@ -5,6 +5,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<EnemySpawnTarget> spawnTargets;
     [SerializeField] private EnemyScript enemyPrefab;
+    [SerializeField] private bool IsNotTutorial = false;
 
     private List<EnemyScript> spawnScripts = new();
 
@@ -22,6 +23,15 @@ public class EnemySpawner : MonoBehaviour
                 new Vector3(target.transform.position.x, target.transform.position.y, 0);
             
             spawnScripts.Add(enemy);
+        }
+
+        if (IsNotTutorial)
+        {
+           foreach(EnemyScript script in spawnScripts) { 
+            
+            script.ActivateEnemy();
+            
+           }
         }
     }
 }
